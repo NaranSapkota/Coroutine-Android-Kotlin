dc8110ff6bd34fe5d964ac8a0e0b40287d681324
Narayan <xanzong.naran29@gmail.com>
2022-04-05 16:59:26 +0545

    runBlocking function call

--- app/src/main/java/com/example/coroutinekotlin/MainActivity.kt
+++ app/src/main/java/com/example/coroutinekotlin/MainActivity.kt
@@ -11,24 +11,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG,"Before the satrting of the run blocking ")
-        runBlocking {
-          Log.d(TAG,"After  the satrting of the run blocking ")
-
-            launch(Dispatchers.IO){
-                delay(3000L)
-                Log.d(TAG,"Finsihed  IO courtine 1")
+        val job=GlobalScope.launch(Dispatchers.Default) {
+            Log.d(TAG,"Starting Long running calculation...")
+            withTimeout(3000L)
+            {
+                for(i in 30..40){
+                    if(isActive){
+                        Log.d(TAG,"Result for i =${fib(i)}")
+                    }
+                }
             }
-
-            launch(Dispatchers.IO){
-                delay(3000L)
-                Log.d(TAG,"Finsihed  IO courtine 2")
-            }
+            Log.d(TAG,"Ending Long running calculation...")
         }
-        Log.d(TAG,"Finsihed  the satrting of the run blocking ")
     }
 
-    // runBlocking is similar to the THread ..It block UI thread nut dispatcher doest block main thread
-    //  launch runs idependently to the runBlocking
+    private fun fib(n: Int): Long {
+        return if(n==0) 0
+        else if(n==1) 1
+        else fib(n-1)+ fib(n-2)
+    }
 }
 
