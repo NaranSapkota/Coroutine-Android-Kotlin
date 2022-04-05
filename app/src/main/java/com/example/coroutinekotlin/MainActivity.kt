f52ea18213b32442168652c27fe1898f0c7624a5
Narayan <xanzong.naran29@gmail.com>
2022-04-05 15:05:43 +0545

    suspend function call

--- app/src/main/java/com/example/coroutinekotlin/MainActivity.kt
+++ app/src/main/java/com/example/coroutinekotlin/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.coroutinekotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        GlobalScope.launch {
+            val networkcallAnswer= doNetworkCall()
+            val networkcallAnswer2= doNetworkCall2()
+            Log.d(TAG,networkcallAnswer)
+            Log.d(TAG,networkcallAnswer2)
+        }
     }
+}
+
+suspend fun doNetworkCall(): String {
+    delay(3000L)
+    return "This is the answer"
+}
+
+suspend fun doNetworkCall2(): String {
+    delay(3000L)
+    return "This is the answer"
 }
