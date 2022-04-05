17178a72fd5fabe9fa64bac8de211edac108c685
Narayan <xanzong.naran29@gmail.com>
2022-04-05 17:31:08 +0545

    await and asynch

--- app/src/main/java/com/example/coroutinekotlin/MainActivity.kt
+++ app/src/main/java/com/example/coroutinekotlin/MainActivity.kt
@@ -3,32 +3,34 @@ package com.example.coroutinekotlin
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
     val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val job=GlobalScope.launch(Dispatchers.Default) {
-            Log.d(TAG,"Starting Long running calculation...")
-            withTimeout(3000L)
-            {
-                for(i in 30..40){
-                    if(isActive){
-                        Log.d(TAG,"Result for i =${fib(i)}")
-                    }
-                }
+        GlobalScope.launch(Dispatchers.IO) {
+            val time = measureTimeMillis {
+                val answer1 = async { networkingCall1() }
+                val answer2 = async { networkingCall2() }
+                Log.d(TAG, "Answer1 is ${answer1.await()}")
+                Log.d(TAG, "Answer2 is ${answer2.await()}")
             }
-            Log.d(TAG,"Ending Long running calculation...")
+            Log.d(TAG, "Request took is ${time}")
         }
+
     }
 
-    private fun fib(n: Int): Long {
-        return if(n==0) 0
-        else if(n==1) 1
-        else fib(n-1)+ fib(n-2)
+    suspend fun networkingCall1(): String {
+        delay(3000L)
+        return "First network call"
+    }
+
+    suspend fun networkingCall2(): String {
+        delay(3000L)
+        return "Second network call"
     }
 }
 
