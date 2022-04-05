9bb1e0c8917d8ad5c09b552487ea6bbcc03cd5d7
Narayan <xanzong.naran29@gmail.com>
2022-04-05 16:07:57 +0545

    suspend function call

--- app/src/main/java/com/example/coroutinekotlin/MainActivity.kt
+++ app/src/main/java/com/example/coroutinekotlin/MainActivity.kt
@@ -3,30 +3,43 @@ package com.example.coroutinekotlin
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     val TAG = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        GlobalScope.launch {
-            val networkcallAnswer= doNetworkCall()
-            val networkcallAnswer2= doNetworkCall2()
-            Log.d(TAG,networkcallAnswer)
-            Log.d(TAG,networkcallAnswer2)
+        //give types of thread in which dispatcher runs
+        GlobalScope.launch(Dispatchers.IO) {
+            //do task
         }
+        //for creating our own  thread
+        GlobalScope.launch(newSingleThreadContext("MyThread")) {
+            //do task
+        }
+        // for creating newteok call using IO Thread and updating UI with chnaging the context of thread UI
+        GlobalScope.launch(Dispatchers.IO) {
+            val networkCall = doNetworkCall()
+            Log.d(TAG, "THread name in IO is  ${Thread.currentThread().name}")
+            //now chnaging it to the Main thread
+            withContext(Dispatchers.Main) {    // with context is use to chnage the thread context
+                tvShowText.text = networkCall
+                Log.d(TAG, "THread name in MAIN  is ${Thread.currentThread().name}")
+            }
+        }
+
     }
 }
 
-suspend fun doNetworkCall(): String {
-    delay(3000L)
-    return "This is the answer"
-}
+// Dispatcher has 4 Thread
+//1. Main thread -> to update the UI
+//2.IO thread -> to perfomr network call or database write read file
+//3.default -> to perform large calculation and perform heavy task
+//4. unconfined --> resume the suspedn thread
 
-suspend fun doNetworkCall2(): String {
+suspend fun doNetworkCall(): String {
     delay(3000L)
     return "This is the answer"
 }
