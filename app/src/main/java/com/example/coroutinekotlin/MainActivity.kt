709f4ab8fc24c69e2fc0dbce140630afc46dbc71
Narayan <xanzong.naran29@gmail.com>
2022-04-05 16:19:35 +0545

    runBlocking function call

--- app/src/main/java/com/example/coroutinekotlin/MainActivity.kt
+++ app/src/main/java/com/example/coroutinekotlin/MainActivity.kt
@@ -11,35 +11,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        //give types of thread in which dispatcher runs
-        GlobalScope.launch(Dispatchers.IO) {
-            //do task
-        }
-        //for creating our own  thread
-        GlobalScope.launch(newSingleThreadContext("MyThread")) {
-            //do task
-        }
-        // for creating newteok call using IO Thread and updating UI with chnaging the context of thread UI
-        GlobalScope.launch(Dispatchers.IO) {
-            val networkCall = doNetworkCall()
-            Log.d(TAG, "THread name in IO is  ${Thread.currentThread().name}")
-            //now chnaging it to the Main thread
-            withContext(Dispatchers.Main) {    // with context is use to chnage the thread context
-                tvShowText.text = networkCall
-                Log.d(TAG, "THread name in MAIN  is ${Thread.currentThread().name}")
+        Log.d(TAG,"Before the satrting of the run blocking ")
+        runBlocking {
+          Log.d(TAG,"After  the satrting of the run blocking ")
+
+            launch(Dispatchers.IO){
+                delay(3000L)
+                Log.d(TAG,"Finsihed  IO courtine 1")
+            }
+
+            launch(Dispatchers.IO){
+                delay(3000L)
+                Log.d(TAG,"Finsihed  IO courtine 2")
             }
         }
-
+        Log.d(TAG,"Finsihed  the satrting of the run blocking ")
     }
+
+    // runBlocking is similar to the THread ..It block UI thread nut dispatcher doest block main thread
+    //  launch runs idependently to the runBlocking
 }
 
-// Dispatcher has 4 Thread
-//1. Main thread -> to update the UI
-//2.IO thread -> to perfomr network call or database write read file
-//3.default -> to perform large calculation and perform heavy task
-//4. unconfined --> resume the suspedn thread
-
-suspend fun doNetworkCall(): String {
-    delay(3000L)
-    return "This is the answer"
-}
